72a60985fc5a16205a0e666b560e98a45b1dc384
maita <josemaitapitado@gmail.com>
2024-11-17 05:28:57 -0300

    creación de directorio de wallpapers y mejora del .sh

--- config_i3wm.sh
+++ config_i3wm.sh
@@ -3,7 +3,7 @@
 # Set of packages and configurations for I3wm to have everything customized to my liking. It only works for Linux Xubuntu.
 
 # Necessary packages to install
-sudo apt update && sudo apt install -y i3 feh rofi brightnessctl flameshot fonts-cascadia-code lxappearance
+sudo apt update && sudo apt install -y feh rofi brightnessctl flameshot fonts-cascadia-code lxappearance
 
 # Necessary permise for brightnessctl work
 sudo usermod -aG video $USER
@@ -11,7 +11,7 @@ sudo usermod -aG video $USER
 # preconfig para el wallpaper
 mkdir -p ~/.config/wallpaper/
 echo "Se creo el directorio de wallpapers."
-cp wallpaper* ~/.config/wallpaper/
+cp wallpapers/wallpaper* ~/.config/wallpaper/
 echo "Se copiaron los wallpapers."
 
 # Copy the i3 config
