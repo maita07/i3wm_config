c4e8d5e6b4627d5b5daa0d72c6ea6b4c75154ffe
maita <josemaitapitado@gmail.com>
2024-11-17 01:35:52 -0300

    Primera confirmación: agregando archivos existentes

---
+++ config_i3wm.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#Necessary packages to install
+sudo apt install -y feh rofi brightnessctl flameshot fonts-cascadia-code lxappearance
+
+#preconfig para el wallpaper
+mkdir -p ~/.config/wallpaper/
+cp wallpaper* ~/.config/wallpaper/
+
+
