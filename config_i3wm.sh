4086163b3cc3bad68c7cf951c26132ba2fa86597
maita <josemaitapitado@gmail.com>
2024-11-17 05:22:43 -0300

    Mejora de la configuración .sh

--- config_i3wm.sh
+++ config_i3wm.sh
@@ -3,14 +3,17 @@
 # Set of packages and configurations for I3wm to have everything customized to my liking. It only works for Linux Xubuntu.
 
 # Necessary packages to install
-sudo apt update && apt install -y i3 feh rofi brightnessctl flameshot fonts-cascadia-code lxappearance
+sudo apt update && sudo apt install -y i3 feh rofi brightnessctl flameshot fonts-cascadia-code lxappearance
 
 # Necessary permise for brightnessctl work
 sudo usermod -aG video $USER
 
 # preconfig para el wallpaper
 mkdir -p ~/.config/wallpaper/
+echo "Se creo el directorio de wallpapers."
 cp wallpaper* ~/.config/wallpaper/
+echo "Se copiaron los wallpapers."
 
 # Copy the i3 config
 cp config ~/.config/i3/
+echo "Se copió la configuración."
