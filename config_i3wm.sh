9e7b0f4bf8e07bba8a916ffee4bda6ec7b329b9d
maita <josemaitapitado@gmail.com>
2024-11-17 04:51:06 -0300

    agregado de los wallpapers

--- config_i3wm.sh
+++ config_i3wm.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
-#Necessary packages to install
-sudo apt install -y feh rofi brightnessctl flameshot fonts-cascadia-code lxappearance
+# Set of packages and configurations for I3wm to have everything customized to my liking. It only works for Linux Xubuntu.
 
-#preconfig para el wallpaper
+# Necessary packages to install
+sudo apt update && apt install -y i3 feh rofi brightnessctl flameshot fonts-cascadia-code lxappearance
+
+# Necessary permise for brightnessctl work
+sudo usermod -aG video $USER
+
+# preconfig para el wallpaper
 mkdir -p ~/.config/wallpaper/
 cp wallpaper* ~/.config/wallpaper/
 
-
+# Copy the i3 config
+cp config ~/.config/i3/
