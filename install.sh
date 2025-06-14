a7e003e2f423a0858ae67319d35f4624bc2fc227
maita07 <josemaitapitado@gmail.com>
2025-06-14 17:06:56 -0300

    new configuration

--- install.sh
+++ install.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Set of packages and configurations for i3wm customized to my liking.
-# This script is intended for Xubuntu.
+# This script is intended for Debian 12.
 
 # Update package lists and install necessary packages
-sudo apt update && sudo apt install -y i3 feh rofi brightnessctl flameshot fonts-dejavu lxappearance
+sudo apt update && sudo apt install -y i3 feh rofi brightnessctl gtk2-engines-murrine flameshot fonts-dejavu lxappearance
 
 # Add current user to the video group for brightness control
 sudo usermod -aG video $USER
@@ -19,3 +19,16 @@ echo "Wallpapers copied."
 mkdir -p ~/.config/i3/
 cp config ~/.config/i3/
 echo "Configuration copied."
+
+# config rofi
+mkdir -p ~/.config/rofi
+cp config_rofi/config.rasi ~/.config/rofi
+cp config_rofi/catppuccin-lavrent-mocha.rasi ~/.config/rofi
+
+# theme
+mkdir -p ~/.themes
+cp -r catppuccin_theme/Catppuccin-Dark-Macchiato ~/.themes
+cp -r catppuccin_theme/Catppuccin-Dark-Macchiato-xhdpi ~/.themes
+cp -r catppuccin_theme/Catppuccin-Dark-Macchiato-hdpi ~/.themes
+cp catppuccin_theme/settings.ini ~/.config/gtk-3.0/
+cp catppuccin_theme/gtk.css ~/.config/gtk-3.0/
